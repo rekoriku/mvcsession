6c3f13d5a1f6da02840e95cbe797d0db72b83ebd
rerikuju <rerikuju@gmail.com>
2019-03-09 16:37:12 +0200

    paths are too hard in php

--- views/includes/header.php
+++ views/includes/header.php
@@ -1,14 +1,12 @@
 <?php 
-include '../../path.php';
-//require_once "$root/isLogged.php";
-echo $path;
+require_once '/home/rekoriku/public_html/serverside19/mvcsession/isLogged.php';
 include_once 'navbar.php';
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Person identity handling<?php echo $path?></title>
+        <title>Person identity handling</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     </head>
     <body>
