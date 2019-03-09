9e4454eb4f26c70897e2aeb2fb4a389eaa953a54
rerikuju <rerikuju@gmail.com>
2019-03-09 16:35:12 +0200

    path

--- views/includes/header.php
+++ views/includes/header.php
@@ -8,7 +8,7 @@ include_once 'navbar.php';
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Person identity handling</title>
+        <title>Person identity handling<?php echo $path?></title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     </head>
     <body>
