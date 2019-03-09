d446f4b214cadb4fbbb542d7522113f8e50d3aec
Viisaspulla <30913249+Viisaspulla@users.noreply.github.com>
2019-03-09 14:15:35 +0200

    Recreated views and includes folders.

---
+++ views/includes/header.php
@@ -0,0 +1,11 @@
+<?php 
+require_once 'isLogged.php';
+$form = new Form($db);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Person identity handling</title>
+    </head>
+    <body>
