d4692e3d30534b97792971513b7d73f0879ebf48
rerikuju <rerikuju@gmail.com>
2019-03-09 16:08:44 +0200

    navbar to header

--- views/includes/header.php
+++ views/includes/header.php
@@ -1,4 +1,7 @@
-<?php require_once '/home/rekoriku/public_html/serverside19/mvcsession/isLogged.php' ?>
+<?php 
+require_once '/home/rekoriku/public_html/serverside19/mvcsession/isLogged.php';
+include_once 'navbar.php';
+?>
 <!DOCTYPE html>
 <html>
     <head>
