8908c1ba9a24539898c433dffd553e7f05ce8305
rerikuju <rerikuju@gmail.com>
2019-03-09 13:46:45 +0200

    Trying to fix autoloader

--- connection.php
+++ connection.php
@@ -1,15 +1,16 @@
 <?php
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-  spl_autoload_register(function ($class_name) {
-    $name = strtolower($class_name);
-    require "models/$class_name.php";
-    require "class/$name.php";
-  });
+spl_autoload_register(function ($class_name) {
+  $directories = ['models/', 'controllers/'];
+  foreach ($directories as $directory) {
+    require "$directory.$class_name.php";
+  }
+});
 
-  $config = parse_ini_file("config.ini"); // Get database information from ini file
-  $db = new Model($config);              // Create new object from the Sqlcon class
-  session_start();
-?>
+$config = parse_ini_file("config.ini"); // Get database information from ini file
+$db = new Model($config);              // Create new object from the Sqlcon class
+session_start();
+ 
