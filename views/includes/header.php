f1a3ba5d049e3c4bd6ae1aafcb5f2b002e18ea37
rerikuju <rerikuju@gmail.com>
2019-03-09 16:26:21 +0200

    test

--- views/includes/header.php
+++ views/includes/header.php
@@ -1,5 +1,6 @@
 <?php 
-require_once __DIR__ . '../../isLogged.php';
+//require_once __DIR__ . '../../isLogged.php';
+echo $_SERVER["DOCUMENT_ROOT"];
 include_once 'navbar.php';
 ?>
 <!DOCTYPE html>
