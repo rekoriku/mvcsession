f96657162f859992857f83a31de6f6f0c99812e3
rerikuju <rerikuju@gmail.com>
2019-03-11 13:25:03 +0200

    no constructor

--- models/PersonModel.php
+++ models/PersonModel.php
@@ -1,11 +1,7 @@
 <?php
 class PersonModel extends Model
 {
-
-  function __construct() {
-
-  }
-
+  
   function getPerson($person) {
     return $this->query("SELECT * FROM serverside19_persons WHERE fname='$person'")->fetch(PDO::FETCH_ASSOC);
   }
