9ace6ec5eaa6771af2eeb8e0a0cd1ba47f5bda38
rerikuju <rerikuju@gmail.com>
2019-03-13 12:39:27 +0200

    deleted unnecessary files and renamed connection.php --> autoload.php

--- links.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'isLogged.php';
-echo "<a href = 'editForm.php'>Edit Accounts</a><br>";
-echo "<a href = 'addForm.php'>Add new accounts</a><br>";
-echo "<a href='logout.php'>Log out!</a>";
-?>
