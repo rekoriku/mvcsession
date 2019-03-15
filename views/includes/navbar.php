08c693a48d82b818c19e2e4b6011a9ea857949ab
Viisaspulla <30913249+Viisaspulla@users.noreply.github.com>
2019-03-15 15:44:44 +0200

    Update navbar.php

--- views/includes/navbar.php
+++ views/includes/navbar.php
@@ -10,13 +10,16 @@
         ?>
     </li>
     <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarPersonDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Person</a>
-        <div class="dropdown-menu" aria-labelledby="navbarPersonDropdownMenuLink">
-          <a class="dropdown-item" href="#">Search</a>
-          <a class="dropdown-item" href="#">Add</a>
-          <a class="dropdown-item" href="#">Edit</a>
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
         </div>
-    </li>
+      </li>
     <li class="nav-item">
       <a class="nav-link" href="editForm.php">Edit Accounts</a>
     </li>
