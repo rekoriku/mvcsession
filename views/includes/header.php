a2ea3eaa8b6f86cb5d9e76d4424ec07105a2ea5c
Viisaspulla <30913249+Viisaspulla@users.noreply.github.com>
2019-03-15 15:49:27 +0200

    Fixed bootstrap.

--- views/includes/header.php
+++ views/includes/header.php
@@ -10,7 +10,7 @@ include_once 'navbar.php';
         <title>Person identity handling</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
