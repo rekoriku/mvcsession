7eb4d8a719729024ded5e75f6b15270dcb0adee9
rerikuju <rerikuju@gmail.com>
2019-03-15 15:47:51 +0200

    removed var dump

--- views/index.php
+++ views/index.php
@@ -14,7 +14,6 @@ $auth = new Authentication;
     </form>
     <?php
       $auth->login();
-      var_dump($auth->priority());
       if(!empty($message))
       {
         echo '<br><div class="alert alert-danger"><strong>'.$message.'</strong> </div>';
