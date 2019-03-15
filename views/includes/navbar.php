c039bc51fb7e9596c1f797d7e0cadbb3d6944354
Viisaspulla <30913249+Viisaspulla@users.noreply.github.com>
2019-03-15 16:01:14 +0200

    Update navbar.php

--- views/includes/navbar.php
+++ views/includes/navbar.php
@@ -1,3 +1,7 @@
+<?php
+  $authentication = new Authentication;
+?>
+
 <nav class="navbar navbar-expand-sm navbar-light" style="background-color: #33A2FF;">
   <!-- Links -->
   <ul class="navbar-nav">
@@ -15,10 +19,18 @@
         </a>
         <div class="dropdown-menu" aria-labelledby="personDropdown">
           <a class="dropdown-item" href="http://niisku.lamk.fi/~rekoriku/serverside19/mvcsession/views/person/getForm.php">Get</a>
-          <a class="dropdown-item" href="http://niisku.lamk.fi/~rekoriku/serverside19/mvcsession/views/person/addForm.php">Add</a>
+          <?php
+            if($authentication->priority() == 0 || $authentication->priority() == 1)
+            {
+              echo '<a class="dropdown-item" href="http://niisku.lamk.fi/~rekoriku/serverside19/mvcsession/views/person/addForm.php">Add</a>"';
+            }
+          ?>
         </div>
     </li>
-    <li class="nav-item dropdown">
+    <?php
+      if($authentication->priority() == 0)
+      {
+        echo '<li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           User
         </a>
@@ -26,7 +38,18 @@
           <a class="dropdown-item" href="http://niisku.lamk.fi/~rekoriku/serverside19/mvcsession/views/user/getForm.php">Get</a>
           <a class="dropdown-item" href="http://niisku.lamk.fi/~rekoriku/serverside19/mvcsession/views/user/addForm.php">Add</a>
         </div>
-    </li>
+      </li>';
+      }
+    ?>
+    <!-- <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          User
+        </a>
+        <div class="dropdown-menu" aria-labelledby="userDropdown">
+          <a class="dropdown-item" href="http://niisku.lamk.fi/~rekoriku/serverside19/mvcsession/views/user/getForm.php">Get</a>
+          <a class="dropdown-item" href="http://niisku.lamk.fi/~rekoriku/serverside19/mvcsession/views/user/addForm.php">Add</a>
+        </div>
+    </li> -->
     <li class="nav-item">
         <a class="nav-link" href="http://niisku.lamk.fi/~rekoriku/serverside19/mvcsession/views/logout.php">Logout</a>
     </li>
