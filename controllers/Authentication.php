ad384de8e3ce802565b5a245635ba92e3479e1db
rerikuju <rerikuju@gmail.com>
2019-03-15 15:08:44 +0200

    redirect

--- controllers/Authentication.php
+++ controllers/Authentication.php
@@ -37,7 +37,7 @@ function priority(){
   if($this->validSess()) {
     return $this->user->getUser(Session::get('username'))['pri'];
   } else {
-    header("location: http://niisku.lamk.fi/~rekoriku/serverside19/mvcsession/views");
+    echo 'invalid session';
   }
 }
 
