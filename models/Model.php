6213b8cf720e2d1513247e593995a2b320e30189
rerikuju <rerikuju@gmail.com>
2019-03-15 14:34:37 +0200

    deleted old commented code

--- models/Model.php
+++ models/Model.php
@@ -22,48 +22,5 @@ class Model {
     $query->execute();
     return $query;
   }
-
-
-
-//   function getUser($user){
-//     return $this->query("SELECT * FROM serverside19_users WHERE user='$user'")->fetch(PDO::FETCH_ASSOC);
-//   }
-
-//   function getAccount($user){
-//     return $this->query(
-//       "SELECT user, description, fname, sname, u.id FROM 
-//       serverside19_users AS u JOIN 
-//       serverside19_persons AS p ON u.id = p.id
-//       WHERE user like '$user'")->fetch(PDO::FETCH_ASSOC);
-//   }
-
-//   function insertUser($id, $fname, $sname, $user, $pwd, $pri, $description) {
-//     try{
-//       $this->pdo->beginTransaction();
-//       $this->query("INSERT INTO serverside19_persons (id, fname, sname) VALUES ('$id', '$fname', '$sname')");
-//       $this->query("INSERT INTO serverside19_users (user, pwd, pri, id, description) VALUES ('$user', '$pwd', '$pri', '$id', '$description')");
-//       $this->pdo->commit();
-//     }
-//     catch(Exception $ex){
-//       echo $ex->getMessage();
-//       $this->pdo->rollBack();
-//     }
-//   }
-
-//   function updateUser($id, $fname, $sname, $user, $description) {
-//     try{
-//       $this->pdo->beginTransaction();
-//       $this->query("UPDATE serverside19_persons SET id='$id', fname='$fname', sname='$sname' WHERE id='$id'");
-//       $this->query("UPDATE serverside19_users SET user='$user', id='$id', description='$description' WHERE id='$id'");
-//       $this->pdo->commit();
-//     } 
-//     catch(Exception $ex){
-//       echo $ex->getMessage();
-//       $this->pdo->rollBack();
-//     }
-// }
-
 }
-
-
 ?>
